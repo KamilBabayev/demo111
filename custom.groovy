e6cab4b176ef2d46df208b77abc6aa9588dc9448
Kamil Babayev <kamilyunis@gmail.com>
2022-04-03 19:40:11 +0400

    Update custom.groovy

--- custom.groovy
+++ custom.groovy
@@ -1,1 +1,13 @@
+def buildApp() {
+    echo "Building application inside GROOVY script"
+}
 
+def testAPP() {
+    echo "testing application insdei Groovy script"
+}
+
+def deployApp() {
+    echo "deploying app inside groovy script"
+}
+
+return this
